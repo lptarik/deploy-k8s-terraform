f2d50d8062727f1024157345413a8ecca9de985e
lptarik <taha.turan@hotmail.com>
2020-04-19 23:00:07 +0300

    Update deployment.tf

--- deployment.tf
+++ deployment.tf
@@ -1,5 +1,40 @@
+provider "aws" {
+  region = "eu-west-1!!!"
+}
+
 provider "kubernetes" {
 }
+
+
+data "aws_eks_cluster" "cluster" {
+  name = "terraform-eks-demo"
+}
+
+output "endpoint" {
+  value = data.aws_eks_cluster.cluster.endpoint
+}
+
+module "metrics_server" {
+  source                                                = "cookielab/metrics-server/kubernetes"
+  version                                               = "0.9.0"
+  metrics_server_option_kubelet_insecure_tls            = true
+  metrics_server_option_kubelet_preferred_address_types = ["InternalIP"]
+}
+
+module "kubernetes_dashboard" {
+  source  = "cookielab/cluster-autoscaler-aws/kubernetes"
+  version = "0.9.0"
+
+  aws_iam_role_for_policy = "terraform-eks-demo-node"
+
+  #data.aws_iam_role.kubernetes_worker_node.name
+
+  asg_tags = [
+    "k8s.io/cluster-autoscaler/enabled",
+    "k8s.io/cluster-autoscaler/${data.aws_eks_cluster.cluster.name}",
+  ]
+}
+
 
 resource "kubernetes_namespace" "application" {
 
@@ -9,16 +44,19 @@ resource "kubernetes_namespace" "application" {
 
 }
 
+
+
 resource "kubernetes_deployment" "nginx" {
   metadata {
-    name = "terraform-example"
+    name      = "terraform-example"
+    namespace = kubernetes_namespace.application.metadata.0.name
     labels = {
       test = "nginx"
     }
   }
 
   spec {
-    replicas = 2
+    replicas = 2!!!
 
     selector {
       match_labels = {
@@ -35,28 +73,31 @@ resource "kubernetes_deployment" "nginx" {
 
       spec {
         container {
-          image = "lptarik/nginx:latest"
+          image = "lptarik/simple-app:latest"
           name  = "nginx"
+          port {
+            container_port = 11130!!!
+          }
 
           resources {
             limits {
-              cpu    = "0.5"
-              memory = "512Mi"
+              cpu    = "0.5!!!"
+              memory = "512Mi!!!"
             }
             requests {
-              cpu    = "250m"
-              memory = "50Mi"
+              cpu    = "250m!!!"
+              memory = "50Mi!!!"
             }
           }
 
           liveness_probe {
             http_get {
               path = "/"
-              port = 80
+              port = 11130!!!
             }
 
-            initial_delay_seconds = 3
-            period_seconds        = 3
+            initial_delay_seconds = 3!!!
+            period_seconds        = 3!!!
           }
         }
       }
@@ -67,23 +108,42 @@ resource "kubernetes_deployment" "nginx" {
 
 resource "kubernetes_service" "nginx-service" {
   metadata {
-    name = "terraform-example"
+    name      = "terraform-example"
+    namespace = kubernetes_namespace.application.metadata.0.name
   }
   spec {
     selector = {
       test = kubernetes_deployment.nginx.metadata.0.labels.test
     }
     port {
-      port        = 80
-      target_port = 80
+      port        = 11130!!!
+      target_port = 11130!!!
     }
 
     type = "LoadBalancer"
   }
 }
 
-#output "lb_ip" {
- # value = kubernetes_service.nginx-service.load_balancer_ingress.0.ip
-#}
+
+resource "kubernetes_horizontal_pod_autoscaler" "hpa" {
+  metadata {
+    name      = kubernetes_deployment.nginx.metadata.0.name
+    namespace = kubernetes_namespace.application.metadata.0.name
+  }
+  spec {
+    max_replicas                      = 10!!!
+    min_replicas                      = 2!!!
+    target_cpu_utilization_percentage = 3!!!
+    scale_target_ref {
+      api_version = "extensions/v1beta1"
+      kind        = "Deployment"
+      name        = kubernetes_deployment.nginx.metadata.0.name
+    }
+  }
+}
+
+output "lb_ip" {
+  value = kubernetes_service.nginx-service.load_balancer_ingress.0.hostname
+}
 
 
